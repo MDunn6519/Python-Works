5f84a92472c0d1a233b4e9eca67558e44387588b
mmd1298 <Dunn65807@missouristate.edu>
2018-01-26 13:11:09 -0600

    New Commit Message

---
+++ dictionaries.py
@@ -0,0 +1,7 @@
+def get_personal_data() -> dict:
+    """"
+    :return: Returns personal data
+    """
+
+    personal_data = {"name": "Matt", "A_Role": "Student"}
+    return personal_data
---
+++ workingfile.py
@@ -0,0 +1,11 @@
+import dictionaries
+
+def main():
+    print("Main")
+    d = dictionaries.get_personal_data()
+    print(d)
+    for key in sorted(d.keys()):
+        print("%s: %s" % (key, d[key]))
+
+if __name__ == '__main__':
+    main()
