5b114d8776407be59ecbac89b73d1dbb8720350b
mmd1298 <Dunn65807@missouristate.edu>
2018-01-29 13:13:29 -0600

    New Commit Message

--- dictionaries.py
+++ dictionaries.py
@@ -1,3 +1,5 @@
+import string
+
 def get_personal_data() -> dict:
     """"
     :return: Returns personal data
@@ -5,3 +7,24 @@ def get_personal_data() -> dict:
 
     personal_data = {"name": "Matt", "A_Role": "Student"}
     return personal_data
+
+
+def main() -> int:
+    default_dict = dict()
+    print(default_dict)
+    initialized_dict = dict([('name', 'matt'), ('a_role', 'student')])
+    print(initialized_dict)
+    simple_init_dict = dict(name='matt', a_role='student')
+    print(simple_init_dict)
+    simple_init_dict['a_role'] = 'joker'
+    print(simple_init_dict)
+    my_comprehension = {x: x**2 for x in range(5)}
+    print(my_comprehension)
+
+    s = "a little lamb, it's fleece".translate({ord(i): None for i in string.punctuation}).split()
+    print(s)
+    return 0
+
+
+if __name__ == '__main__':
+    main()
